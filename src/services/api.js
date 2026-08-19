66602b5a60b1fb7be09f3672115dcd566e2674fa
Qalib137 <qalibelwkberov@gmail.com>
2026-08-19 19:16:42 +0400

    fix api base url to render

--- src/services/api.js
+++ src/services/api.js
@@ -1,17 +1,20 @@
 import axios from "axios";
 
+const API_URL = import.meta.env.MODE === "production"
+    ? "https://restaurant-backend-35x5.onrender.com"
+    : "http://localhost:4000";
+
 const api = axios.create({
-    baseURL: "https://restaurant-backend-35x5.onrender.com",
+    baseURL: API_URL,
 });
 
 api.interceptors.request.use((config) => {
     const token = localStorage.getItem("token");
-
     if (token) {
         config.headers.Authorization = `Bearer ${token}`;
     }
-
     return config;
 });
 
+export const BASE_URL = API_URL;
 export default api;
